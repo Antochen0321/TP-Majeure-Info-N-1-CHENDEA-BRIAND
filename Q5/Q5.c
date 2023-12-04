bd5b66fc684e32698c9b758ea08ac4b217058fc8
Antochen0321 <130651941+Antochen0321@users.noreply.github.com>
2023-12-04 10:55:51 +0100

    Update Q5.c
    Q5, only code wich have an impact on this question
    
    
    Signed-off-by: Antochen0321 <130651941+Antochen0321@users.noreply.github.com>

--- Q5/Q5.c
+++ Q5/Q5.c
@@ -1,21 +1,3 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include <time.h>
-
-#define MAX_COMMAND_LENGTH 100
-
-
-/*Welcom function*/
-void welcome_message() {
-    write(1, "Welcome to the ENSEA Shell.\n", 28);
-    write(1, "To exit, type 'exit'\n", 22);
-    /*Here, we write the command to print message.(1) for standard output, then the string,
-    then the string size*/
-}
 
 /*Function to execute our command*/
 void execute_command(char *command) {
@@ -29,28 +11,8 @@ void execute_command(char *command) {
     /*Creation of a new process with fork()*/
     pid = fork();
 
-    if (pid == 0) {
-        // son process
-        //Here, we execute the command in son process thanks to execlp
-        if (execlp(command, command, (char *)NULL) == -1) {
-            //If there is an error, we display an error message
-            perror("enseash");
-            //Exit of the son process with the failure code
-            _exit(EXIT_FAILURE);
-        }
-    }
-
-    else if (pid < 0) {
-        //If there is an error, we display an error message
-        perror("enseash");
-    } 
-    
-    else {
-        // parent process
-        //Waiting for the execute end of the command in son process
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    if (pid == 0) {//code of the Q1-2-3 Questions
+        ...
     }
 
     //end of time measure
@@ -84,41 +46,4 @@ void execute_command(char *command) {
         write(1, elapsed_time_str, strlen(elapsed_time_str));
         write(1, "ms] ", 5);
     }
-}
-
-//main function
-int main(int argc, char *argv[]) {
-
-    char user_input[MAX_COMMAND_LENGTH];
-
-    //display of the welcom message
-    welcome_message();
-
-    while (1) {
-        // Display prompt for user input
-        write(1, "enseash % ", 11);
-
-        // Use of read function to read user input
-        ssize_t bytes_read = read(0, user_input, sizeof(user_input));
-
-        // delete of line break at command end
-        // without this command, "enseash %" stay display even after have write "exit"
-        user_input[strcspn(user_input, "\n")] = 0;
-    
-        //verification of the exit command to leave the shell
-        if (strcmp(user_input, "exit") == 0 || bytes_read == 0) {
-            // Display of departure message
-            write(1, "Good bye!\n", 11);
-            // Exit of the infinite loop
-            break;
-        }
-
-        // Execution of user input command 
-        execute_command(user_input);
-
-    }
-    
-
-    // Fin du programme
-    return 0;
 }
