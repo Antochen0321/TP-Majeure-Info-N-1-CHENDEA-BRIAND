7ded4db258b287bbcd6c7d952b2483b938179cb4
Antochen0321 <130651941+Antochen0321@users.noreply.github.com>
2023-12-04 09:20:20 +0100

    Update Q4.c
    Signed-off-by: Antochen0321 <130651941+Antochen0321@users.noreply.github.com>

--- Q4/Q4.c
+++ Q4/Q4.c
@@ -6,6 +6,8 @@
         // if the process finished normally
         write(1, "[exit:", 6);
         char exit_status_str[5];
+        // the sheel send an exit message with the value of the memory block used by the call after the exit. 
+        //Especialy 1 when the call generate an error like 'No such file directory'
         snprintf(exit_status_str, sizeof(exit_status_str), "%d", WEXITSTATUS(status));
         write(1, exit_status_str, strlen(exit_status_str));
         write(1, "] ", 2);
@@ -13,6 +15,7 @@
         // if the process was interrupted by un signap
         write(1, "[sign:", 6);
         char signal_number_str[5];
+        //the sheel will send a sign message when the call generate a segmentation false error like
         snprintf(signal_number_str, sizeof(signal_number_str), "%d", WTERMSIG(status));
         write(1, signal_number_str, strlen(signal_number_str));
         write(1, "] ", 2);
